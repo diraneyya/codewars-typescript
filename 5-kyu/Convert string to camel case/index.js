c401b9b3ec66c401478c1791b577e6913e224766
Orwa <orwa.diraneyya@wildcodeschool.com>
2022-10-18 09:41:07 +0200

    Commited solutions

--- 5-kyu/Convert string to camel case/index.js
+++ 5-kyu/Convert string to camel case/index.js
@@ -23,7 +23,9 @@
     https://www.codewars.com/kata/convert-string-to-camel-case/solutions
 */
 // Short Solution. Regex here => https://regexr.com/3gtji
-const toCamelCase = function () {
+const toCamelCase = function (param) {
+    let counter = 0;
+    return param.split(/[-_]/).map(e => (counter++ ? e.charAt(0).toUpperCase() : e.charAt(0)) + e.substring(1)).join('');
 };
 // Function Export
 module.exports = toCamelCase;
